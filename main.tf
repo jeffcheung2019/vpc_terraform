33912f3bc90bd9de1873d18a709bbf5308918330
Cheung jeff <cheungjeff@Cheungs-MacBook-Pro.local>
2022-10-18 15:37:07 +0800

    az only 2 available

--- main.tf
+++ main.tf
@@ -17,9 +17,9 @@ module "vpc" {
   name = local.name
   cidr = "10.1.0.0/16"
 
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
-  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
+  azs             = ["${local.region}b", "${local.region}c"]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24"]
+  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24"]
 
   enable_ipv6 = false
   enable_nat_gateway = false
