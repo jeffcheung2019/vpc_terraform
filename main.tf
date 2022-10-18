9d23490afa338f3ab29d22babcc17c8886aef65a
Cheung jeff <cheungjeff@Cheungs-MacBook-Pro.local>
2022-10-18 14:35:12 +0800

    vpc

---
+++ main.tf
@@ -0,0 +1,36 @@
+locals {
+  name   = "bastion-host"
+  region = "us-west-1"
+
+  tags = {
+    Name = local.name
+  }
+}
+
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = local.name
+  cidr = "10.1.0.0/16"
+
+  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
+
+  enable_ipv6 = false
+  enable_nat_gateway = false
+
+  public_subnet_tags = {
+    Name = "public"
+  }
+
+  tags = local.tags
+
+  vpc_tags = {
+    Name = "bastion-vpc"
+  }
+}
---
+++ versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 0.13.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.73"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
